0a012a5a5ef715419fab7e306029f6824a13972a
Illusionnist <goldchief@yahoo.com>
2025-05-16 17:54:09 +1000

    Navbar and Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import NavBar from "./components/NavBar.jsx";
+import LogoSection from "./components/LogoSection.jsx";
 
 
 const App = () => {
     return (
         <>
+            <NavBar/>
             <Hero/>
             <ShowcaseSection/>
+            {/*<LogoSection/>*/}
         </>
 
     )
